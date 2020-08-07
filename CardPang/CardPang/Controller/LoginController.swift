7cfd539ab98453c61528e091029303730b4b7ded
ByoungilYoun <jimmy7525@naver.com>
2020-08-07 14:33:47 +0900

    Prepare Project

---
+++ CardPang/CardPang/Controller/LoginController.swift
@@ -0,0 +1,22 @@
+//
+//  LoginController.swift
+//  CardPang
+//
+//  Created by 윤병일 on 2020/08/06.
+//  Copyright © 2020 Byoungil Youn. All rights reserved.
+//
+
+import UIKit
+
+class LoginController : UIViewController {
+  
+  //MARK: - Properties
+  
+  //MARK: - LifeCycle
+  
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    view.backgroundColor = .red
+   
+  }
+}
