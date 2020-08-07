cd2aba4eae9d1d954ecf6023e74cf262dc52296a
ByoungilYoun <jimmy7525@naver.com>
2020-08-08 04:12:00 +0900

    configureUI()

--- CardPang/CardPang/Controller/LoginController.swift
+++ CardPang/CardPang/Controller/LoginController.swift
@@ -16,7 +16,20 @@ class LoginController : UIViewController {
   
   override func viewDidLoad() {
     super.viewDidLoad()
-    view.backgroundColor = .red
-   
+    configureUI()
+  }
+  
+  private func configureUI() {
+    navigationController?.navigationBar.isHidden = true
+    navigationController?.navigationBar.barStyle = .black
+    
+    
+    let upperColor = UIColor(red: 205/255.0, green: 53/255.0, blue: 134/255.0, alpha: 1.0)
+    let lowerColor = UIColor(red: 233/255.0, green: 171/255.0, blue: 67/255.0, alpha: 1.0)
+    let gradient = CAGradientLayer()
+    gradient.colors = [upperColor.cgColor, lowerColor.cgColor]
+    gradient.locations = [0, 1]
+    gradient.frame = view.frame
+    view.layer.addSublayer(gradient)
   }
 }
